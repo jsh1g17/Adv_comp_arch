b6bf4317b875cc084e851e44eb8636ff947b9597
jsh1g17 <33068040+jsh1g17@users.noreply.github.com>
2020-02-26 15:44:15 +0000

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,138 +1,99 @@
 #bin/bash
 
-echo "x86 test program"
+echo "test program"
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=2kB --l1i_size=2kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_2.txt
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=1 --cacheline_size=16 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=2kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_1_16_2_2.txt
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_2.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=8kB --l1i_size=2kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=1 --cacheline_size=32 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_2.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_1_32_2_2.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=16kB --l1i_size=2kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=1 --cacheline_size=64 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_2.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_1_64_2_2.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=32kB --l1i_size=2kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=1 --cacheline_size=128 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_32_2.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_1_128_2_2.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=64kB --l1i_size=2kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=2 --cacheline_size=16 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_64_2.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_16_2_2.txt
 
-echo "test 2"
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=2kB --l1i_size=4kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=2 --cacheline_size=32 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_4.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_32_2_2.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=4kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=2 --cacheline_size=64 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_4.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_64_2_2.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=8kB --l1i_size=4kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=2 --cacheline_size=128 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_4.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_128_2_2.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=16kB --l1i_size=4kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=4 --cacheline_size=16 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_4.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_16_2_2.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=32kB --l1i_size=4kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_32_4.txt
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=4 --cacheline_size=32 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=64kB --l1i_size=4kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_32_2_2.txt
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_64_4.txt
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=4 --cacheline_size=64 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-echo "test 4"
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_64_2_2.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=2kB --l1i_size=8kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=4 --cacheline_size=128 --caches --l1d_size=2kB --l1i_size=2kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_8.txt
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_128_2_2.txt
 
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=8kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_8.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=8kB --l1i_size=8kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_8.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=16kB --l1i_size=8kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_8.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=32kB --l1i_size=8kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_32_8.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=64kB --l1i_size=8kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_64_8.txt
-
-echo "test 8"
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=2kB --l1i_size=16kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_16.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=16kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_16.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=8kB --l1i_size=16kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_16.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=16kB --l1i_size=16kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_16.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=32kB --l1i_size=16kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_32_16.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=64kB --l1i_size=16kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_64_16.txt
-
-echo "test 16"
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=2kB --l1i_size=32kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_32.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=4kB --l1i_size=32kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_4_32.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=8kB --l1i_size=32kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_8_32.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=16kB --l1i_size=32kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_16_32.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=32kB --l1i_size=32kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_32_32.txt
-
-./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU  --caches --l1d_size=64kB --l1i_size=32kB --option="/usr/local/src/gem5/automotive/susan/input_small.pgm -s" -c ./automotive/susan/susan
-
-cp ./m5out/stats.txt ./Adv_comp_arch/stats_64_32.txt
 
 echo "test 32"
 
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=1 --cacheline_size=16 --caches --l1d_size=16kB --l1i_size=16kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
 
-echo "x86 test completed"
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_1_16_16_16.txt
 
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=1 --cacheline_size=32 --caches --l1d_size=16kB --l1i_size=16kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_1_32_16_16.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=1 --cacheline_size=64 --caches --l1d_size=16kB --l1i_size=16kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_1_64_16_16.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=1 --cacheline_size=128 --caches --l1d_size=16kB --l1i_size=16kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_1_128_16_16.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=2 --cacheline_size=16 --caches --l1d_size=16kB --l1i_size=16kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_16_16_16.txt
+
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=2 --cacheline_size=32 --caches --l1d_size=16kB --l1i_size=16kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_32_16_16.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=2 --cacheline_size=64 --caches --l1d_size=16kB --l1i_size=16kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_64_16_16.txt
+
+./build/X86/gem5.opt ./configs/example/se.py --cpu-type=TimingSimpleCPU --l1d_assoc=2 --cacheline_size=128 --caches --l1d_size=16kB --l1i_size=16kB --option=/usr/local/src/gem5/automotive/susan/input_small.pgm -s -c /usr/local/src/gem5/automotive/susan/susan
+
+cp ./m5out/stats.txt ./Adv_comp_arch/stats_2_128_16_16.txt
+
+echo "test 16"
+
+
+
+echo "test completed"
 
